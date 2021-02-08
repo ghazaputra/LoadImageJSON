ebe9d0f11f235c284569608e2e2f090aaa542396
ghazaputra <70355895+ghazaputra@users.noreply.github.com>
2021-02-08 13:17:09 +0700

    Add files via upload

---
+++ LoadImage JSON/LoadImage JSON/Model/News.swift
@@ -0,0 +1,16 @@
+//
+//  News.swift
+//  LoadImage JSON
+//
+//  Created by ADMIN on 08/02/21.
+//
+
+import Foundation
+
+struct News : Identifiable {
+    let id = UUID()
+    let title : String
+    let description : String
+    let image : String
+}
+
